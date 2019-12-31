c64fa989ed6c40aa461f50e8336745f97f82b3d0
yappy <superunyo@yahoo.co.jp>
2019-12-31 18:53:35 +0900

    Search tempreture string and convert to float

--- py/fan.py
+++ py/fan.py
@@ -1,5 +1,11 @@
 import subprocess
+import re
 
-stdout = subprocess.run(['vcgencmd', 'measure_temp'],
-	check=True, capture_output=True).stdout
-print(stdout.decode())
+# [0-9]+ "." [0-9]+
+temp_re = re.compile(r'\d+\.\d+')
+
+temp_str = subprocess.run(['vcgencmd', 'measure_temp'],
+	check=True, capture_output=True).stdout.decode()
+m = temp_re.search(temp_str)
+temp = float(m.group())
+print(temp)
