9d87f3831ce3e51a94cff970a4e06faaf53dc311
yappy <superunyo@yahoo.co.jp>
2019-12-31 19:06:44 +0900

    Placeholder for polling loop

--- py/fan.py
+++ py/fan.py
@@ -1,11 +1,35 @@
+import time
 import subprocess
 import re
+
+config = {
+	# polling loop wait
+	'period': 1.0,
+	# thre
+	'threshold': 40.0,
+}
 
 # [0-9]+ "." [0-9]+
 temp_re = re.compile(r'\d+\.\d+')
 
-temp_str = subprocess.run(['vcgencmd', 'measure_temp'],
-	check=True, capture_output=True).stdout.decode()
-m = temp_re.search(temp_str)
-temp = float(m.group())
-print(temp)
+def get_temp():
+	temp_str = subprocess.run(['vcgencmd', 'measure_temp'],
+		check=True, capture_output=True).stdout.decode()
+	m = temp_re.search(temp_str)
+	temp = float(m.group())
+	return temp
+
+def main_loop():
+	temp = get_temp()
+	print(temp)
+	if temp >= config['threshold']:
+		pass
+	else:
+		pass
+
+def main():
+	while True:
+		main_loop()
+		time.sleep(config['period'])
+
+main()
